c04bfd40eca7a41a3f15a37b13dca983c6cd8508
lika <likaeristavi8@gmail.com>
2022-07-13 18:27:02 +0400

    update_assignment1

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,7 @@
 #%%
 import pandas as pd
+# %%
+import numpy as np
 #%%
 df = pd.read_csv ('armenian_pubs_csv.csv')
 
@@ -7,4 +9,18 @@ df = pd.read_csv ('armenian_pubs_csv.csv')
 df.shape 
 # %%
 df
+# %%
+df.info()
+# %%
+df.head()
+
+# %%
+# What is the most favourite pub?
+from collections import Counter
+df = pd.read_csv ('armenian_pubs_csv.csv')
+occurences = Counter(df['Fav_Pub'])
+most_used_word = max(occurences, key = occurences.get)
+# %%
+# What is the average age and income of the respondents?
+df.mean(axis='index')
 # %%
